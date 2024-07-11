7fa6a8ca0cbee41c3682a899c28e622e6c402311
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-07-10 21:25:24 -0400

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,8 @@
+export function isOverlapping(elem1, elem2) {
+    const rect1 = elem1.getBoundingClientRect();
+    const rect2 = elem2.getBoundingClientRect();
+    return !(rect1.right < rect2.left || 
+             rect1.left > rect2.right || 
+             rect1.bottom < rect2.top || 
+             rect1.top > rect2.bottom);
+}
